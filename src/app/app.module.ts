391e74cc213ee073eabb3e778b08552e7f365329
al <alprihodko@gmail.com>
2017-11-24 16:33:15 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { RelayComponent } from './relay/relay.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RelayComponent
   ],
   imports: [
     BrowserModule
