7d81c4ca353b5b973888518aeb5fe59aaf83967b
al <alprihodko@gmail.com>
2017-12-27 19:15:27 +0200

    adding material to system

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RelayComponent } from './relay/relay.component';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +17,18 @@ import { RelayComponent } from './relay/relay.component';
     RelayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
